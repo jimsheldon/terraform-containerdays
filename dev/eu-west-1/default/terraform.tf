677a75cb364c1763d4b9683320b22528d964fa95
Jim Sheldon <jim.sheldon@meltwater.com>
2020-07-16 19:24:30 -0400

    adding find changes (#5)
    * adding find changes

--- dev/eu-west-1/default/terraform.tf
+++ dev/eu-west-1/default/terraform.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "terraform-state-jimcontainerdays-demo"
     key    = "eu-west-1/default.tfstate"
     region = "eu-west-1"
+
     // workaround for https://github.com/terraform-providers/terraform-provider-aws/issues/5018
     skip_metadata_api_check = true
   }
