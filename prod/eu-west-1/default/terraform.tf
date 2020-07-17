e2cd27debfe7dc6b1e3013c3493734de69be7a91
Jim Sheldon <jim.sheldon@meltwater.com>
2020-07-17 10:18:07 -0400

    fixes (#6)
    paths for dev and prod

--- prod/eu-west-1/default/terraform.tf
+++ prod/eu-west-1/default/terraform.tf
@@ -1,16 +1,12 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-jimcontainerdays-demo"
+    bucket = "terraform-state-jimcontainerdays-demoprod"
     key    = "eu-west-1/default.tfstate"
     region = "eu-west-1"
 
     // workaround for https://github.com/terraform-providers/terraform-provider-aws/issues/5018
     skip_metadata_api_check = true
   }
-}
-
-terraform {
-  required_version = ">= 0.12"
 }
 
 provider "aws" {
