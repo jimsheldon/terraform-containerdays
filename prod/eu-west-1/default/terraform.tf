0e69efa1fb6aaad1bd5144b7b60909826201693b
Jim Sheldon <jim.sheldon@meltwater.com>
2020-07-17 09:48:48 -0400

    add prod

---
+++ prod/eu-west-1/default/terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-jimcontainerdays-demoprod"
+    key    = "eu-west-1/default.tfstate"
+    region = "eu-west-1"
+
+    // workaround for https://github.com/terraform-providers/terraform-provider-aws/issues/5018
+    skip_metadata_api_check = true
+  }
+}
+
+terraform {
+  required_version = ">= 0.11"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  version = "~> 2.0"
+}
+
+provider "template" {
+  version = "~> 2.0"
+}
+
